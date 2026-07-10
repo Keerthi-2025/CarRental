ff78562d2c832fc817428165525d3a9f3a787a10
Keerthi <rhkeerthi3@gmail.com>
2026-07-10 21:40:32 +0530

    created User entity

---
+++ CarRental/src/main/java/com/carRental/CarRental/entites/User.java
@@ -0,0 +1,49 @@
+package com.carRental.CarRental.entites;
+
+
+import com.carRental.CarRental.RoleType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Getter
+@Setter
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID user_id;
+
+    @Column(nullable = false)
+    private  String firstName;
+
+    @Column(nullable = false)
+    private  String lastName;
+
+    @Column(nullable = false, unique = true)
+    private  String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String phone;
+
+    private  String profileImage;
+
+    private  String DL;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoleType role;
+
+    private  Boolean enabled = false;
+}
