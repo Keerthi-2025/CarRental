3c9c5324d24638b05be33d4c396fcc561eb3ca7f
Keerthi <rhkeerthi3@gmail.com>
2026-07-12 01:47:50 +0530

    added otpverification entity

---
+++ CarRental/src/main/java/com/carRental/CarRental/entites/OtpVerification.java
@@ -0,0 +1,42 @@
+package com.carRental.CarRental.entites;
+
+
+import com.carRental.CarRental.OtpPurpose;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "otp_verification")
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
+
+public class OtpVerification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "otp_id")
+    private UUID otpId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private  String otp;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OtpPurpose otpPurpose;
+
+    @Column(nullable = false)
+    private LocalDate expiryTime;
+
+    @Column(nullable = false)
+    private  Boolean verified = false;
+}
