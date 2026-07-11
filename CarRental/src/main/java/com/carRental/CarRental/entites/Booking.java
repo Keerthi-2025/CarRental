9bde52f9ee91c0b1bad610e260958b9775d44866
Keerthi <rhkeerthi3@gmail.com>
2026-07-11 21:36:04 +0530

    added Booking entity

---
+++ CarRental/src/main/java/com/carRental/CarRental/entites/Booking.java
@@ -0,0 +1,57 @@
+package com.carRental.CarRental.entites;
+
+
+import com.carRental.CarRental.BookingStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "bookings")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+
+@Getter
+@Setter
+
+public class Booking {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID bookingId;
+
+    @Column(nullable = false)
+    private LocalDate pickupDate;
+
+    @Column(nullable = false)
+    private LocalDate returnDate;
+
+    @Column(nullable = false)
+    private  String pickupLocation;
+
+    @Column(nullable = false)
+    private  String dropLocation;
+
+    @Column(nullable = false)
+    private BigDecimal totalAmt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BookingStatus bookingStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private  User user;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id", nullable = false)
+    private  Car car;
+
+
+}
