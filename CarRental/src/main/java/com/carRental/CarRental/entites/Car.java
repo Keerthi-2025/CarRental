55cc43911cc352dfd4255509758a6bdf0c9dff63
Keerthi <rhkeerthi3@gmail.com>
2026-07-11 21:14:10 +0530

    added carImage entity

--- CarRental/src/main/java/com/carRental/CarRental/entites/Car.java
+++ CarRental/src/main/java/com/carRental/CarRental/entites/Car.java
@@ -4,8 +4,7 @@ package com.carRental.CarRental.entites;
 import com.carRental.CarRental.CarStatus;
 import com.carRental.CarRental.FuelType;
 import com.carRental.CarRental.Transmission;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -22,39 +21,48 @@ import java.util.UUID;
 
 public class Car {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "car_id")
     private UUID carId;
 
-
+    @Column(nullable = false)
     private String carName;
 
+    @Column(nullable = false)
     private String brand;
 
+    @Column(nullable = false)
     private  String model;
 
+    @Column(nullable = false)
     private  Integer year;
 
-
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private FuelType fuelType;
 
-
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Transmission transmission;
 
-
+    @Column(nullable = false)
     private Integer seatingCapacity;
 
-
+    @Column(nullable = false)
     private BigDecimal pricePerDay;
 
+    @Column(nullable = false, unique = true)
     private  String registrationNumber;
 
-
+    @Column(nullable = false)
     private  String color;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CarStatus carStatus;
 
-
-    private CarStatus cartStatus;
-
-
+    @Column(length = 1000)
     private  String description;
 
 
---
+++ CarRental/src/main/java/com/carRental/CarRental/entites/CarImage.java
@@ -0,0 +1,32 @@
+package com.carRental.CarRental.entites;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "car_images")
+@Builder
+
+
+@Getter
+@Setter
+
+public class CarImage {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "car_id")
+    private UUID imageId;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private  Car car;
+
+}
