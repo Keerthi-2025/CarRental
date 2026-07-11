689ef85e75ca19f11727b2938602f2a57d9cc7ca
Keerthi <rhkeerthi3@gmail.com>
2026-07-12 01:42:48 +0530

    added otpurpose enum

---
+++ CarRental/src/main/java/com/carRental/CarRental/OtpPurpose.java
@@ -0,0 +1,7 @@
+package com.carRental.CarRental;
+
+public enum OtpPurpose {
+
+    REGISTER,
+    RESET_PASSWORD
+}
--- CarRental/src/main/java/com/carRental/CarRental/entites/Payment.java
+++ CarRental/src/main/java/com/carRental/CarRental/entites/Payment.java
@@ -31,7 +31,7 @@ public class Payment {
     private  PaymentStatus paymentGateway;
 
     @Column
-    private  String gatewatPaymentId;
+    private  String gatewayPaymentId;
 
     @Column(nullable = false)
     private BigDecimal amt;
