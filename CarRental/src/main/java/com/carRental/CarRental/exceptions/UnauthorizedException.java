63744417d4fc13e3121b824dc95ac6dd5c3f7d77
Keerthi <rhkeerthi3@gmail.com>
2026-07-13 15:36:07 +0530

    added UnauthorizedException

---
+++ CarRental/src/main/java/com/carRental/CarRental/exceptions/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package com.carRental.CarRental.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+}
