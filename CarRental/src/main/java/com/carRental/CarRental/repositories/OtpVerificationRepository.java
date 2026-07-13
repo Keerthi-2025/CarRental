1e353259f0204334065e384401c828a93b7b7643
Keerthi <rhkeerthi3@gmail.com>
2026-07-13 16:58:33 +0530

    added OtpVerificationRepository

---
+++ CarRental/src/main/java/com/carRental/CarRental/repositories/OtpVerificationRepository.java
@@ -0,0 +1,20 @@
+package com.carRental.CarRental.repositories;
+
+
+import com.carRental.CarRental.entites.OtpVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+
+public interface OtpVerificationRepository extends JpaRepository<OtpVerification, UUID> {
+
+
+    Optional<OtpVerification> findByEmail(String email);
+
+
+    void deleteNyEmail(String email);
+}
