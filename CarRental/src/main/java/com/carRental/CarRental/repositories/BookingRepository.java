7e1831abc657b104225b886f3aa45a483b2fec5b
Keerthi <rhkeerthi3@gmail.com>
2026-07-13 16:54:01 +0530

    added BookingRepository

---
+++ CarRental/src/main/java/com/carRental/CarRental/repositories/BookingRepository.java
@@ -0,0 +1,21 @@
+package com.carRental.CarRental.repositories;
+
+
+import com.carRental.CarRental.entites.Booking;
+import com.carRental.CarRental.entites.Car;
+import com.carRental.CarRental.entites.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.awt.print.Book;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+
+public interface BookingRepository  extends JpaRepository<Booking, UUID> {
+
+    List<Booking> findByUser(User user);
+
+    List<Booking> findByCar(Car car);
+}
--- CarRental/src/main/java/com/carRental/CarRental/repositories/CarRepository.java
+++ CarRental/src/main/java/com/carRental/CarRental/repositories/CarRepository.java
@@ -3,13 +3,17 @@ package com.carRental.CarRental.repositories;
 import com.carRental.CarRental.CarStatus;
 import com.carRental.CarRental.entites.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+
+@Repository
+
 public interface CarRepository extends JpaRepository<Car, UUID> {
 
-    List<Car> findByBrandContainingIgnorecase(String brand);
+    List<Car> findByBrandContainingIgnoreCase(String brand);
 
     List<Car> findByCarNameContainingIgnoreCase(String carName);
 
