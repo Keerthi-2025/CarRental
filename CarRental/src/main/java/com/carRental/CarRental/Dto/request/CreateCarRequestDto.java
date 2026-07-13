ece2304a222c4eefe1bbc7b3624e30702e0a880d
Keerthi <rhkeerthi3@gmail.com>
2026-07-13 20:11:17 +0530

    added CreateCarRequestDto

---
+++ CarRental/src/main/java/com/carRental/CarRental/Dto/request/CreateCarRequestDto.java
@@ -0,0 +1,21 @@
+package com.carRental.CarRental.Dto.request;
+
+import com.carRental.CarRental.Transmission;
+
+import java.math.BigDecimal;
+
+public record CreateCarRequestDto(
+        String carName,
+        String brand,
+        String model,
+        Integer year,
+        String fuelType,
+        Transmission transmission,
+        Integer seatingCapacity,
+        BigDecimal pricePerDay,
+        String registrationNumber,
+        String color,
+        String description
+
+) {
+}
