26c825564ce1dd36fc4dde19fc18955d865093ba
Keerthi <rhkeerthi3@gmail.com>
2026-07-14 21:01:05 +0530

    added UserMapper

---
+++ CarRental/src/main/java/com/carRental/CarRental/mappers/UserMapper.java
@@ -0,0 +1,24 @@
+package com.carRental.CarRental.mappers;
+
+
+import com.carRental.CarRental.RoleType;
+import com.carRental.CarRental.entites.User;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class UserMapper {
+
+    public User toUser(String firstName, String lastName, String email, String password, String phone, RoleType role) {
+        return User.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .email(email)
+                .password(password)
+                .phone(phone)
+                .role(role)
+                .build();
+    }
+
+
+}
