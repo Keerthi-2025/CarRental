accc839e96790e241a60a30b54013bc5c4448647
Keerthi <rhkeerthi3@gmail.com>
2026-07-14 21:13:20 +0530

    completed PaymentMapper

--- CarRental/src/main/java/com/carRental/CarRental/mappers/PaymentMapper.java
+++ CarRental/src/main/java/com/carRental/CarRental/mappers/PaymentMapper.java
@@ -1,4 +1,22 @@
 package com.carRental.CarRental.mappers;
 
+
+import com.carRental.CarRental.PaymentGateway;
+import com.carRental.CarRental.PaymentStatus;
+import com.carRental.CarRental.entites.Payment;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+
 public class PaymentMapper {
+    public Payment toPayment(BigDecimal amt, PaymentGateway paymentGateway, String gatewayPaymentId){
+        return Payment.builder()
+                .amt(amt)
+                .paymentGateway(paymentGateway)
+                .gatewayPaymentId(gatewayPaymentId)
+                .paymentStatus(PaymentStatus.PENDING)
+                .build();
+    }
 }
