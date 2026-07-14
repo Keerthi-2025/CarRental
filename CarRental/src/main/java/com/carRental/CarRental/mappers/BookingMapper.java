2d680919673efe8ed9dfc8a8db2f718bf04374ad
Keerthi <rhkeerthi3@gmail.com>
2026-07-14 21:10:41 +0530

    completed BookingMapper

--- CarRental/src/main/java/com/carRental/CarRental/mappers/BookingMapper.java
+++ CarRental/src/main/java/com/carRental/CarRental/mappers/BookingMapper.java
@@ -1,4 +1,29 @@
 package com.carRental.CarRental.mappers;
 
+
+import com.carRental.CarRental.BookingStatus;
+import com.carRental.CarRental.entites.Booking;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Component
+
 public class BookingMapper {
+    public Booking toBooking(
+            LocalDate pickupDate,
+            LocalDate returnDate,
+            String pickupLocation,
+            String dropLocation,
+            BigDecimal totalAmt){
+        return Booking.builder()
+                .pickupDate(pickupDate)
+                .returnDate(returnDate)
+                .pickupLocation(pickupLocation)
+                .dropLocation(dropLocation)
+                .totalAmt(totalAmt)
+                .bookingStatus(BookingStatus.PENDING)
+                .build();
+    }
 }
