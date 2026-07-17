7aa8c2de8aeaed68ff675e67fdf2ec7540281d7b
Keerthi <rhkeerthi3@gmail.com>
2026-07-17 17:26:46 +0530

    added EmailService

--- CarRental/src/main/java/com/carRental/CarRental/services/EmailService.java
+++ CarRental/src/main/java/com/carRental/CarRental/services/EmailService.java
@@ -1,4 +1,27 @@
 package com.carRental.CarRental.services;
 
-public class EmailService {
+
+public interface EmailService {
+
+    String sendOtpEmail(String toEmail, String subject, String otp);
+
+    String sendWelcomeEmail(String toEmail, String firstName);
+
+    String sendBookingConfirmationEmail(
+            String toEmail,
+            String firstName,
+            String bookingId
+    );
+
+    String sendBookingCancellationEmail(
+            String toEmail,
+            String firstName,
+            String bookingId
+    );
+
+    String sendPaymentSuccessEmail(
+            String toEmail,
+            String firstName,
+            String paymentId
+    );
 }
